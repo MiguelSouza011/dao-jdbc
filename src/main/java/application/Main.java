3c67a0a14c0b1a6d2f42bcc6e4562646d9e1283f
Miguel Souza <souzzamiguel09@gmail.com>
2026-08-19 18:52:08 -0300

    Reusing intantiation

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -10,14 +10,9 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        //Department obj = new Department(1, "Books");
-
-        //Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        //System.out.println(obj);
-        //System.out.println(seller);
+        System.out.println("==== TEST 1: seller findById ====");
 
         Seller seller = sellerDao.findById(3);
 
