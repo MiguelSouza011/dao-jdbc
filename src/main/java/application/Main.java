4df9cb443e586a4aa69667b829e490ef7adb55d7
Miguel Souza <souzzamiguel09@gmail.com>
2026-08-20 11:42:56 -0300

    DepartmentDao implementation and test

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -16,22 +17,28 @@ public class Main {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+
+        System.out.println();
         System.out.println("==== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
+        System.out.println();
         System.out.println("\n==== TEST 2: seller findByDepartment ====");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println();
         System.out.println("\n==== TEST 3: seller findAll ====");
         list = sellerDao.findAll();
         for (Seller obj : list) {
             System.out.println(obj);
         }
 
+        System.out.println();
         System.out.println("\n==== TEST 4: seller insert ====");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
         sellerDao.insert(newSeller);
@@ -50,5 +57,6 @@ public class Main {
         int id = sc.nextInt();
         sellerDao.deleteById(id);
         System.out.println("Delete completed");
+
     }
 }
